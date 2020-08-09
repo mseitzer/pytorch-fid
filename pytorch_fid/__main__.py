adfc834c860a90558adf0a6978c6b7ab0593daef
mseitzer <16725193+mseitzer@users.noreply.github.com>
2020-08-09 18:20:02 +0200

    Small changes to setup mechanism
    Add entry points, rename package to use underscore, small fixes.

---
+++ pytorch_fid/__main__.py
@@ -0,0 +1,4 @@
+import pytorch_fid.fid_score
+
+
+pytorch_fid.fid_score.main()
--- setup.py
+++ setup.py
@@ -1,28 +1,37 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+
 setuptools.setup(
-    name="pytorch-fid", # Replace with your own username
-    version="1.0.0",
-    author="Maximilian Seitzer",
-    author_email="maximilian.seitzer@tuebingen.mpg.de",
-    description="Package for calculating Frechet Inception Distance using PyTorch",
+    name='pytorch-fid',
+    version='1.0.0',
+    author='Max Seitzer',
+    author_email='contact@max-seitzer.de',
+    description=('Package for calculating Frechet Inception Distance (FID) '
+                 'using PyTorch'),
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/mseitzer/pytorch-fid",
+    long_description_content_type='text/markdown',
+    url='https://github.com/mseitzer/pytorch-fid',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0 License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'pytorch-fid = pytorch_fid.fid_score:main',
+        ],
+    },
     install_requires=[
-        "torch",
-        "torchvision",
-        "numpy",
-        "scipy"
+        'numpy',
+        'pillow',
+        'scipy',
+        'torch',
+        'torchvision'
     ]
 )
