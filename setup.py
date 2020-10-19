4ce1a3b5eee64157b54d3f1ab52062864e384c03
mseitzer <16725193+mseitzer@users.noreply.github.com>
2020-10-19 17:39:49 +0200

    Retrieve version from package __init__

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
+import os
 import setuptools
 
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+def read(rel_path):
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base_path, rel_path), 'r') as f:
+        return f.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+
+    raise RuntimeError('Unable to find version string.')
 
 
 setuptools.setup(
     name='pytorch-fid',
-    version='0.1.1',
+    version=get_version('src/pytorch_fid/__init__.py'),
     author='Max Seitzer',
     author_email='current.address@unknown.invalid',
     description=('Package for calculating Frechet Inception Distance (FID) '
                  'using PyTorch'),
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://github.com/mseitzer/pytorch-fid',
-    packages=setuptools.find_packages('src/'),
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src/'),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
