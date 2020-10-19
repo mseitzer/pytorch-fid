48bb34eb808b764dbf703d04f938ef921fbffe8b
mseitzer <16725193+mseitzer@users.noreply.github.com>
2020-10-19 17:34:23 +0200

    Move package to src/ folder

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/mseitzer/pytorch-fid',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src/'),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Apache Software License',
