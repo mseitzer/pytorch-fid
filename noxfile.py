457f3fed9225a3b8b371c3a97ba5ea88ce76ff1b
mseitzer <16725193+mseitzer@users.noreply.github.com>
2024-03-16 13:45:25 +0100

    Install torch cpu version for tests

--- noxfile.py
+++ noxfile.py
@@ -15,6 +15,11 @@ def lint(session):
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
+    session.install(
+        'torch==2.2.1', 
+        'torchvision', 
+        '--index-url', 'https://download.pytorch.org/whl/cpu'
+    )
     session.install('.')
     session.install('pytest')
     session.install('pytest-mock')
