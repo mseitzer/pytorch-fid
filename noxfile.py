88d0de4992cae0d2829549e0b2cb7312639110b3
mseitzer <16725193+mseitzer@users.noreply.github.com>
2024-03-16 13:43:43 +0100

    Run Nox tests with different Python versions

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ def lint(session):
     session.run('flake8', *args)
 
 
-@nox.session
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     session.install('.')
     session.install('pytest')
