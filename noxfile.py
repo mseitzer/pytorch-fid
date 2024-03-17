50d816ef00ef96d8f3400a89a89f11766a508469
mseitzer <16725193+mseitzer@users.noreply.github.com>
2024-03-17 14:00:35 +0100

    Run nox sessions through poetry versions

--- noxfile.py
+++ noxfile.py
@@ -1,29 +1,39 @@
+import sys
+from textwrap import dedent
+
 import nox
+
+
+try:
+    from nox_poetry import session
+except ImportError:
+    message = f"""\
+    Nox failed to import the 'nox-poetry' package.
+
+    Please install it using the following command:
+
+    {sys.executable} -m pip install nox-poetry"""
+    raise SystemExit(dedent(message)) from None
+
 
 LOCATIONS = ("src/", "tests/", "noxfile.py", "setup.py")
 
 
-@nox.session
+@session
 def lint(session):
     session.install("flake8")
     session.install("flake8-bugbear")
     session.install("flake8-isort")
-    session.install("black==24.3.0")
+    session.install("black")
 
     args = session.posargs or LOCATIONS
     session.run("flake8", *args)
     session.run("black", "--check", "--diff", *args)
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
-    session.install(
-        "torch==2.2.1",
-        "torchvision",
-        "--index-url",
-        "https://download.pytorch.org/whl/cpu",
-    )
-    session.install(".")
+    session.install(".", '--extra-index-url', 'https://download.pytorch.org/whl/cpu')
     session.install("pytest")
     session.install("pytest-mock")
     session.run("pytest", *session.posargs)
