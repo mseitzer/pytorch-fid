98fbcc5966235719ee65447ad0c947fbd099dfd8
mseitzer <16725193+mseitzer@users.noreply.github.com>
2024-03-17 14:26:54 +0100

    Run linting through pre-commit

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,11 @@
+import os
+import shlex
 import sys
+from pathlib import Path
 from textwrap import dedent
 
 try:
-    from nox_poetry import session
+    from nox_poetry import Session, session
 except ImportError:
     message = f"""\
     Nox failed to import the 'nox-poetry' package.
@@ -15,21 +18,108 @@ except ImportError:
 LOCATIONS = ("src/", "tests/", "noxfile.py")
 
 
-@session
-def lint(session):
-    session.install("flake8")
-    session.install("flake8-bugbear")
-    session.install("flake8-isort")
-    session.install("black")
+def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
+    """Activate virtualenv in hooks installed by pre-commit.
 
-    args = session.posargs or LOCATIONS
-    session.run("flake8", *args)
-    session.run("black", "--check", "--diff", *args)
+    This function patches git hooks installed by pre-commit to activate the
+    session's virtual environment. This allows pre-commit to locate hooks in
+    that environment when invoked from git.
+
+    This function is copied from cookiecutter-hypermodern-python, see
+    https://github.com/cjolowicz/cookiecutter-hypermodern-python/blob/main/%7B%7Bcookiecutter.
+    project_name%7D%7D/noxfile.py
+
+    Args:
+        session: The Session object.
+    """
+    assert session.bin is not None  # nosec
+
+    # Only patch hooks containing a reference to this session's bindir. Support
+    # quoting rules for Python and bash, but strip the outermost quotes so we
+    # can detect paths within the bindir, like <bindir>/python.
+    bindirs = [
+        bindir[1:-1] if bindir[0] in "'\"" else bindir
+        for bindir in (repr(session.bin), shlex.quote(session.bin))
+    ]
+
+    virtualenv = session.env.get("VIRTUAL_ENV")
+    if virtualenv is None:
+        return
+
+    headers = {
+        # pre-commit < 2.16.0
+        "python": f"""\
+            import os
+            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
+            os.environ["PATH"] = os.pathsep.join((
+                {session.bin!r},
+                os.environ.get("PATH", ""),
+            ))
+            """,
+        # pre-commit >= 2.16.0
+        "bash": f"""\
+            VIRTUAL_ENV={shlex.quote(virtualenv)}
+            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
+            """,
+        # pre-commit >= 2.17.0 on Windows forces sh shebang
+        "/bin/sh": f"""\
+            VIRTUAL_ENV={shlex.quote(virtualenv)}
+            PATH={shlex.quote(session.bin)}"{os.pathsep}$PATH"
+            """,
+    }
+
+    hookdir = Path(".git") / "hooks"
+    if not hookdir.is_dir():
+        return
+
+    for hook in hookdir.iterdir():
+        if hook.name.endswith(".sample") or not hook.is_file():
+            continue
+
+        if not hook.read_bytes().startswith(b"#!"):
+            continue
+
+        text = hook.read_text()
+
+        if not any(
+            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
+            for bindir in bindirs
+        ):
+            continue
+
+        lines = text.splitlines()
+
+        for executable, header in headers.items():
+            if executable in lines[0].lower():
+                lines.insert(1, dedent(header))
+                hook.write_text("\n".join(lines))
+                break
+
+
+@session
+def precommit(session):
+    args = session.posargs or [
+        "run",
+        "--all-files",
+        "--hook-stage=manual",
+        "--show-diff-on-failure",
+    ]
+    session.install(
+        "black",
+        "flake8",
+        "flake8-bugbear",
+        "isort",
+        "pre-commit",
+        "pre-commit-hooks",
+        "pyupgrade",
+    )
+    session.run("pre-commit", *args)
+    if args and args[0] == "install":
+        activate_virtualenv_in_precommit_hooks(session)
 
 
 @session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     session.install(".", "--extra-index-url", "https://download.pytorch.org/whl/cpu")
-    session.install("pytest")
-    session.install("pytest-mock")
+    session.install("pytest", "pytest-mock")
     session.run("pytest", *session.posargs)
